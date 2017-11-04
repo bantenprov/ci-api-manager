3f67710e72f3e4038907b80d11b3dd28869b84a5
masadi <masadi.com@gmail.com>
2017-11-04 14:18:26 +0700

    Add files via upload

---
+++ application/core/MY_Model.php
@@ -0,0 +1,7 @@
+<?php
+include_once "CI_Base_Model.php";
+class MY_Model extends CI_Base_Model{
+    protected function get_current_user(){
+        return $this->session->userdata('user_id');
+    }
+}
